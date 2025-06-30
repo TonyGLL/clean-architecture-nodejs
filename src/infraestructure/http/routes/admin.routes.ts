4621c8a0c6a80b7acfd8b20043a8ba811ccb01b4
TonyGLL <tonygllambia@gmail.com>
2025-06-30 17:42:40 -0600

    Ajuste permisos

--- src/infraestructure/http/routes/admin.routes.ts
+++ src/infraestructure/http/routes/admin.routes.ts
@@ -5,21 +5,20 @@ import { permissionsMiddleware } from '../middlewares/permissions.middleware';
 import userRoutes from './user.routes';
 import catalogsRoutes from './catalogs.routes';
 import authAdminRouter from './auth.admin.routes';
-import modulesRoutes from './modules.routes';
 
 const mainAdminRouter = Router();
 
-mainAdminRouter.use('/auth', authAdminRouter);
+mainAdminRouter
+    .use('/auth', authAdminRouter)
 
-// Apply authMiddleware to all subsequent routes in mainAdminRouter
-mainAdminRouter.use(authMiddleware);
-// Apply permissionsMiddleware after authMiddleware for all subsequent routes
-mainAdminRouter.use(permissionsMiddleware);
+    // Middleware to check authentication and permissions
+    .use(authMiddleware)
+    // Middleware to check permissions for admin routes
+    .use(permissionsMiddleware)
 
-// Protected routes
-mainAdminRouter.use('/roles', roleRoutes);
-mainAdminRouter.use('/catalogs', catalogsRoutes);
-mainAdminRouter.use('/users', userRoutes);
-mainAdminRouter.use('/modules', modulesRoutes);
+    // Protected routes for admin
+    .use('/roles', roleRoutes)
+    .use('/catalogs', catalogsRoutes)
+    .use('/users', userRoutes);
 
 export default mainAdminRouter;
