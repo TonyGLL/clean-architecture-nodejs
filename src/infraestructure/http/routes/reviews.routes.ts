e8ed1d0e985cee84c2dbebde16e291c8a3b8c7d0
TonyGLL <tonygllambia@gmail.com>
2025-09-04 17:13:10 -0600

    Create review

--- src/infraestructure/http/routes/reviews.routes.ts
+++ src/infraestructure/http/routes/reviews.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { ReviewsController } from '../controllers/reviews.ctrl';
 import { container } from '../../ioc/config';
-import { GetProductReviewsValidator } from '../validators/reviews.validator';
+import { CreateReviewValidator, GetProductReviewsValidator } from '../validators/reviews.validator';
 import { expressValidatorErrors } from '../middlewares/validator.middleware';
 
 const router = Router();
@@ -9,5 +9,6 @@ const controller = container.get<ReviewsController>(ReviewsController);
 
 router
     .get('/:productId', GetProductReviewsValidator, expressValidatorErrors, controller.getProductReviews)
+    .post('/:productId', CreateReviewValidator, expressValidatorErrors, controller.createReview)
 
 export default router;
--- src/infraestructure/http/validators/reviews.validator.ts
+++ src/infraestructure/http/validators/reviews.validator.ts
@@ -1,4 +1,4 @@
-import { param } from "express-validator";
+import { body, param } from "express-validator";
 import { pageLimitQueryValidator } from "./general.validator";
 
 export const GetProductReviewsValidator = [
@@ -11,4 +11,29 @@ export const GetProductReviewsValidator = [
         .bail()
         .isInt({ min: 1 }).withMessage('Param `productId` must be a positive integer')
         .toInt()
+];
+
+export const CreateReviewValidator = [
+    param('productId')
+        .exists().withMessage('Param `productId` is required')
+        .bail()
+        .notEmpty().withMessage('Param `productId` cannot be empty')
+        .bail()
+        .isInt({ min: 1 }).withMessage('Param `productId` must be a positive integer')
+        .toInt(),
+    body('rating')
+        .exists().withMessage('Field `rating` is required')
+        .bail()
+        .notEmpty().withMessage('Field `rating` cannot be empty')
+        .bail()
+        .isInt({ min: 1, max: 5 }).withMessage('Field `rating` must be an integer between 1 and 5')
+        .toInt(),
+    body('body')
+        .exists().withMessage('Field `body` is required')
+        .bail()
+        .isString().withMessage('Field `body` must be a string')
+        .bail()
+        .notEmpty().withMessage('Field `body` cannot be empty if provided')
+        .bail()
+        .trim()
 ];
